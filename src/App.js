8934f01d590d80d42811b05946ffa95b48915908
Scott McCulley <smcculley@identitymind.com>
2020-02-18 21:55:06 -0800

    updated body copy

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          I was changed on the Feature branch
+          I was changed on the Feature branch<br>
+          And, I was deployed through travis ci!
         </p>
       </div>
     );
